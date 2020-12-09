c5883df8ee8110e08d6572298a7b5ec9c153ace4
Dele O <717638+deletosh@users.noreply.github.com>
2020-12-09 11:58:52 -0500

    Adds the starting point for our validator

---
+++ app.js
@@ -0,0 +1,18 @@
+const username = document.getElementById('username')
+// const email = document.getElementById("email")
+
+const usernameValue = username.value
+
+// 2. Check if the value is empty
+// console.log(usernameValue)
+// 3. If empty, give user some feedback
+// conditionals
+if (usernameValue === '') {
+//   // what should happen
+  console.log('Please enter your username!')
+
+//   alert("Please enter your username!")
+}
+
+
+// 4. Other, it's OK.
---
+++ random-sidebar.js
@@ -0,0 +1,23 @@
+// window.alert("Some random message")
+// prompt("")
+
+const todaysDate = prompt("What's today?")
+
+alert(todaysDate)
+
+// window > document > bunch of calls
+
+// Browser environment
+// Chrome (V8)
+// Firefox (SpiderM)
+// Edge (Shakara)
+
+// // Non-browser
+// Backend / Servers (Node.js / V8)
+// Robotics (Johnny 5)
+// Artificial Intelligence (Tensorflow.js)
+// > > Vision Learning
+// Internet-Of-Things,  (J5)
+
+// Application Programming Interface (API)
+// >>
