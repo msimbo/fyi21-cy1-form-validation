6c6043316136de67fbdfb350807bd4e029715b27
Audra <73372336+IamAudra@users.noreply.github.com>
2020-12-10 16:09:19 -0500

    Edited form validtation file

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // 1. get DOM element
 const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
+const password = document.getElementById('password')
+const email = document.getElementById('email')
 const usernameErr = username.nextElementSibling
+const passwordErr = password.nextElementSibling
+const emailErr = email.nextElementSibling
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -16,9 +20,19 @@ regForm.addEventListener('click', function (e) {
   } else {
     console.log('sending to server')
   }
-
+ //@TODO: add the validation for email
+ if (password.value === '') {
+   passwordErr.className = 'block bg-red-600 text-white'
+ }
+ else { console.log('sending to server')}
   //@TODO: add the validation for email
+
 
   //@TODO: add the validation for password
 
+if (email.value === '') {
+  emailErr.className = 'block bg-red-500 text-white'
+} else {
+  console.log('sending to server')
+}
 })
