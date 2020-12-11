c20294e69c30721f7749c815432960da24501145
Dele O <717638+deletosh@users.noreply.github.com>
2020-12-11 12:15:39 -0500

    Adds blur error checking on username

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 // 1. get DOM element
 const regForm = document.getElementById('registration-form')
+
 const username = document.getElementById('username')
 const usernameErr = username.nextElementSibling
-// const usernameErr = document.getElementById("usernameErr")
+
+username.addEventListener('blur', function (e) {
+  if (username.value !== '') {
+    e.target.nextElementSibling.innerHTML = '<small class="success">✔ Complete</small>'
+  }
+})
 
 // 2. Add event / modification
-
 regForm.addEventListener('click', function (e) {
   e.preventDefault()
 
   // 2. Check if the value is empty
   if (username.value === '') {
     // 3. If empty, give user some feedback
-    usernameErr.className = 'block bg-red-500 text-white'
+    usernameErr.innerHTML = '<small class="error"> ❌ Please enter your username </small>'
   } else {
-    console.log('sending to server')
+    console.log('Sending to server')
   }
-
   //@TODO: add the validation for email
 
   //@TODO: add the validation for password
-
 })
+
+function showError(){
+  // what should be done...
+}
+
