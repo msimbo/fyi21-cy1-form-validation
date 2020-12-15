0cefe919e143ad4b1a5bfbefe19818046bf4167f
Dele O <717638+deletosh@users.noreply.github.com>
2020-12-15 12:30:04 -0500

    Wraps up form validations
    - Refactor the form event listeners
    - Adds functions for other validators
    - Starting point for Code Challenge 5c
    
    See https://jsitor.com/xoVMMhA_t for sandbox and experimentation.

--- app.js
+++ app.js
@@ -1,48 +1,82 @@
 // 1. get DOM element
-const regForm = document.getElementById('registration-form')
+const form = document.getElementById('registration-form')
 
 const username = document.getElementById('username')
 const email = document.getElementById('email')
 
-regForm.addEventListener('submit', function (e) {
+// adding event listener on the form
+form.addEventListener('submit', function (e) {
   e.preventDefault()
 
   validateEmpty(username)
+  validateEmpty(email)
+  // validateMinLength(username)
+  // validateMinLength(username)
+  //validateEmpty(password)
+  // validateFirstName(firstName)
+  // validateIsEmail(email)
 
   //@TODO: add the validation for email (Code Challenge 5a)
   //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
+  //@TODO: Code Challenge 5c: Starting from your refactored code,
+  // 1. Implement and use the validatePassMatch() function to confirm both passwords match
+  // 2. Implement and use the emailValidates() function to confirm it's a valid email. See: https://stackoverflow.com/a/46181
 
   //@TODO: add the validation for password (Code Challenge 5a)
   //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
 })
 
-function validateEmpty(input) {
-  console.log(input)
+// input: HTMLInputElement
+function validateEmpty (input) {
+  const inputName = input.name
   if (input.value === '') {
-      showError(input)
+    showError(input, `${inputName} is empty`)
   } else {
-      showSuccess(input)
+    showSuccess(input)
   }
 }
 
-function showError(input){
-  // steps to do this...
-  console.log('input is empty')
+// input: HTMLInputElement (DOM) | msg: string
+function showError (input, msg) {
+  input.nextElementSibling.innerHTML = `<small class="error">${msg}</small>`
+
+  //reverts the success border
+  input.className='base-input'
 }
 
-function showSuccess () {
-    console.log('you are ready to submit')
+function showSuccess (input) {
+  input.className = 'success-input'
+  // removes the flash message
+  input.nextElementSibling.innerHTML = ''
 }
 
-function validatePassMatch(password1, password2){
+function validatePassMatch (password1, password2) {
+  // password1.value === password2.value
   //@TODO: check if the passwords match
 }
 
-function validateIsEmail(email){
-  //@TODO: check if input is an email
+function validateIsEmail (email) {
+  if (emailValidates(email)) { // return true
+    showSuccess(email)
+  } else {
+    showError(email, 'Email is invalid')
+  }
 }
 
+// returns boolean
+function emailValidates (email) {
+  // Regular Expression
+  //@TODO:  find implementation on SO
+  //
+  return true
+}
 
-function validateMinLength(input){
-  //@TODO: check length
+// input: HTMLInputElement
+function validateMinLength (input) {
+  // console.log(input.value.length)
+  if (input.value.length < 6) {
+    showError(input, 'Username too short')
+  } else {
+    showSuccess(input)
+  }
 }
